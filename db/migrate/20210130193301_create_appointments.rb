1bad11f556ee85589288c30d5c6a26940601bd51
elisa <elisaopalka@gmail.com>
2021-01-30 14:53:02 -0500

    migrated tables

--- db/migrate/20210130193301_create_appointments.rb
+++ db/migrate/20210130193301_create_appointments.rb
@@ -1,7 +1,10 @@
 class CreateAppointments < ActiveRecord::Migration[6.0]
   def change
     create_table :appointments do |t|
-      t.datetime :time
+      t.datetime :datetime
+      t.integer :doctor_id
+      t.integer :patient_id
+
 
       t.timestamps
     end
--- db/migrate/20210130193337_create_patients.rb
+++ db/migrate/20210130193337_create_patients.rb
@@ -2,6 +2,8 @@ class CreatePatients < ActiveRecord::Migration[6.0]
   def change
     create_table :patients do |t|
       t.string :name
+      t.integer :age
+      t.date :dob
 
       t.timestamps
     end
